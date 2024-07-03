841071d8e714409f8f897da5d971756e8700604a
Stefano Violante <ste.violante@gmail.com>
2024-07-03 12:27:24 +0200

    Updade

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main_interactive():
         code = get_multiline_input("Inserisci il codice Python:")
         #output_file = input(
         #    "Inserisci il nome del file di output (default: formatted_code.jsonl): ").strip() or "formatted_code.jsonl"
-        output_file = '/SecureCatalogue.jsonl'   
+        output_file = './SecureCatalogue.jsonl'   
     elif choice == "2":
         file_path = input("Inserisci il percorso del file contenente il codice: ").strip()
         #output_file = input(
